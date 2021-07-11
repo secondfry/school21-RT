c99a6a1ce1e778c9ca11d7ed77b0e08b38986541
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-07-11 17:14:38 +0300

    [intersection_plane.c] norminette

--- srcs/intersection_plane.c
+++ srcs/intersection_plane.c
@@ -1,6 +1,8 @@
 #include "intersection_plane.h"
 
-// https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
+// https://www.scratchapixel.com/
+// lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/
+// ray-plane-and-ray-disk-intersection
 
 // (C - P) dot N = 0
 // O + D * t = P
@@ -10,35 +12,51 @@
 // (C - O) dot N - t * D dot N = 0
 // t = (C - O) dot N / D dot N
 
-static float	intersection_plane(t_rtv *rtv, t_intersect_params *params, t_byte idx)
+static float	intersection_plane(
+	t_rtv *rtv,
+	t_intersect_params *params,
+	t_byte idx
+)
 {
-	// (C - O) dot normal / D dot normal
-	t_vector_4 C = rtv->planes[idx].position;
-	float b = vector_dot(rtv->planes[idx].normal, params->D);
+	const t_vector_4	C = rtv->planes[idx].position;
+	const t_vector_4	CO = vector_sub(C, params->O);
+	const float			a = vector_dot(rtv->planes[idx].normal, CO);
+	const float			b = vector_dot(rtv->planes[idx].normal, params->D);
 
-	// if (b < EPSILON && b > -1 * EPSILON) {
-	// 	return 1.0 / 0.0;
-	// }
-
-	t_vector_4 CO = vector_sub(C, params->O);
-	float a = vector_dot(rtv->planes[idx].normal, CO);
-	return a / b;
+	if (b < EPSILON && b > -1 * EPSILON)
+	{
+		return (1.0 / 0.0);
+	}
+	return (a / b);
 }
 
-t_intersection	intersection_plane_closest(t_rtv *rtv, t_intersect_params *params)
+t_intersection	intersection_plane_closest(
+	t_rtv *rtv,
+	t_intersect_params *params
+)
 {
-	float t_closest = 1.0 / 0.0;
-	t_byte idx = -1;
-	float t;
+	float	t_closest;
+	t_byte	idx;
+	t_byte	i;
+	float	t;
 
-	for (t_byte i = 0; i < MAX_PLANES; i++) {
+	t_closest = 1.0 / 0.0;
+	idx = -1;
+	i = 0;
+	while (i < MAX_PLANES)
+	{
 		if (!(rtv->planes[i].traits & TRAIT_EXISTS))
-			continue;
+		{
+			i++;
+			continue ;
+		}
 		t = intersection_plane(rtv, params, i);
-		if (t > params->t_min && t < params->t_max && t < t_closest) {
+		if (t > params->t_min && t < params->t_max && t < t_closest)
+		{
 			t_closest = t;
 			idx = i;
 		}
+		i++;
 	}
-	return ((t_intersection) { t_closest, idx, IPLANE });
+	return ((t_intersection){t_closest, idx, IPLANE});
 }
