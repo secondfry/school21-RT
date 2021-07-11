4e08601752273f546e6a2dfebdf619da045d66f9
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-07-11 15:51:50 +0300

    [init_rtv.h] norminette

--- includes/init_rtv.h
+++ includes/init_rtv.h
@@ -1,11 +1,13 @@
-#ifndef INIT_RTV
-# define INIT_RTV
+#ifndef INIT_RTV_H
+# define INIT_RTV_H
 
 # include "defines.h"
 # include "vector.h"
 # include "matrix_factory.h"
 # include "color.h"
 
-void init_rtv(t_rtv *rtv);
+void	init_rtv_objects(t_rtv *rtv);
+void	init_rtv_lights(t_rtv *rtv);
+void	init_rtv(t_rtv *rtv);
 
 #endif
