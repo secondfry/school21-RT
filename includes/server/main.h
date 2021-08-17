451adf4f7b2e43c7f94269da9bc11aababe39b62
Rustam @SecondFry Gubaydullin <secondfry+github@gmail.com>
2021-08-17 21:50:42 +0300

    Parallel execution on RT.server

--- includes/server/main.h
+++ includes/server/main.h
@@ -4,6 +4,18 @@
 # include <czmq.h>
 # include "init_rtv.h"
 
+typedef struct s_params {
+	t_rtv	*rtv;
+	int		*data;
+	short	ya;
+	short	yb;
+	short	xa;
+	short	xb;
+}				t_params;
+
+# define THREAD_COUNT 8
+
 t_byte	handle_typed_message(zsock_t *reader, t_rtv *rtv, char *id);
+t_byte	handle_render(t_rtv *rtv, zsock_t *reader);
 
 #endif
