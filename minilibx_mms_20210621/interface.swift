cc67b8af36e7a73ade7024f9fc56fed42e58b46a
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-08-22 15:38:17 +0300

    Make CGAssociateMouseAndMouseCursorPosition argument conditional

--- minilibx_mms_20210621/interface.swift
+++ minilibx_mms_20210621/interface.swift
@@ -270,7 +270,13 @@ public func mlx_mouse_move_swift(_ winptr:UnsafeRawPointer, _ x:Int32, _ y:Int32
 	var pt = CGPoint(x:CGFloat(x), y:CGFloat(y))
 	pt = win.convertToDisplay(pt)
 	CGWarpMouseCursorPosition(pt)
+
+#if swift(>=5.3)
 	CGAssociateMouseAndMouseCursorPosition(Int32(1))
+#else
+	CGAssociateMouseAndMouseCursorPosition(UInt32(1))
+#endif
+
 	return Int32(0);
 }
 
