0624aaa7b3769822f6350acd671d39ad20fae20b
Rustam @SecondFry Gubaydullin <secondfry+github@gmail.com>
2021-08-28 15:19:59 +0300

    Remove MLX headers

--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: oadhesiv <secondfry+school21@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 16:00:22 by oadhesiv          #+#    #+#             */
-/*   Updated: 2021/07/03 00:05:56 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2021/08/28 15:19:42 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include <unistd.h>
 # include <mlx.h>
 # include "defines.h"
-# include "init_mlx.h"
 # include "init_rtv.h"
 # include "graceful.h"
 # include "vector.h"
