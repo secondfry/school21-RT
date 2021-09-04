b9640dc0746f47fbaacb713dcddbc36b3318f91f
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-09-04 20:40:01 +0300

    [init_sdl.c] norminette

--- srcs/sdl/init_sdl.c
+++ srcs/sdl/init_sdl.c
@@ -6,7 +6,7 @@
 /*   By: secondfry <secondfry@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 13:56:04 by oadhesiv          #+#    #+#             */
-/*   Updated: 2021/09/04 20:36:23 by secondfry        ###   ########.fr       */
+/*   Updated: 2021/09/04 20:39:41 by secondfry        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,63 @@
 #include "init_sdl.h"
 #include "loop_hook.h"
 
-int	sdl_init(t_sdl *sdl)
-{
-	// Setup SDL
-	// (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
-	// depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version of SDL is recommended!)
-	int status = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER);
-	if (status != 0)
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-		return (status);
-	}
-
-	// Decide GL+GLSL versions
 #if defined(__APPLE__)
-	// GL 3.2 Core + GLSL 150
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+// GL 3.2 Core + GLSL 150
+static void	set_platform_attributes(void)
+{
+	SDL_GL_SetAttribute(\
+		SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(\
+		SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+}
+
 #else
-	// GL 3.0 + GLSL 130
+
+// GL 3.0 + GLSL 130
+static void	set_platform_attributes(void)
+{
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(\
+		SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+}
+
 #endif
 
-	// Create window with graphics context
+static void	set_common_attributes(void)
+{
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-	SDL_DisplayMode DM;
-	SDL_GetCurrentDisplayMode(0, &DM);
-	sdl->window = SDL_CreateWindow("RT", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DM.w, DM.h, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+}
+
+int	sdl_init(t_sdl *sdl)
+{
+	const int		status = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+	SDL_DisplayMode	dm;
+
+	if (status != 0)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, \
+			"Couldn't initialize SDL: %s", SDL_GetError());
+		return (status);
+	}
+	set_platform_attributes();
+	set_common_attributes();
+	SDL_GetCurrentDisplayMode(0, &dm);
+	sdl->window = SDL_CreateWindow("RT", \
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, dm.w, dm.h, \
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 	ft_ptr_check(sdl->window, SDL_GetError(), 0);
 	sdl->gl_context = SDL_GL_CreateContext(sdl->window);
 	ft_ptr_check(sdl->gl_context, SDL_GetError(), 0);
 	SDL_GL_MakeCurrent(sdl->window, sdl->gl_context);
-	SDL_GL_SetSwapInterval(1); // Enable vsync
-
-	return 0;
+	SDL_GL_SetSwapInterval(1);
+	return (0);
 }
 
 void	sdl_clear_texture(t_sdl *sdl)
@@ -73,7 +89,10 @@ t_byte	sdl_handle_event(t_sdl *sdl)
 		ImGui_ImplSDL2_ProcessEvent(&event);
 		if (event.type == SDL_QUIT)
 			return (1);
-		if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(sdl->window))
+		if (event.type == SDL_WINDOWEVENT \
+			&& event.window.event == SDL_WINDOWEVENT_CLOSE \
+			&& event.window.windowID == SDL_GetWindowID(sdl->window) \
+		)
 			return (1);
 		if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
 			return (1);
@@ -93,7 +112,6 @@ void	sdl_loop(t_rtv *rtv, t_sdl *sdl)
 		if (sdl_handle_event(sdl))
 			break ;
 		loop_hook(rtv);
-		// sdl_draw(sdl);
 	}
 }
 
@@ -108,52 +126,42 @@ void	sdl_clear(t_sdl *sdl)
 	SDL_Quit();
 }
 
-void	imgui_init(t_sdl *sdl, t_imgui *imgui)
-{
-	// Decide GL+GLSL versions
 #if defined(__APPLE__)
-	// GL 3.2 Core + GLSL 150
-	const char* glsl_version = "#version 150";
+
+// GL 3.2 Core + GLSL 150
+static void	imgui_opengl_platform_init(void)
+{
+	const char	*glsl_version = "#version 150";
+
+	ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
 #else
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
+
+// GL 3.0 + GLSL 130
+static void	imgui_opengl_platform_init(void)
+{
+	const char	*glsl_version = "#version 130";
+
+	ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
 #endif
 
-	// Setup Dear ImGui context
-	igDebugCheckVersionAndDataLayout("1.84.1", sizeof(ImGuiIO), sizeof(ImGuiStyle), sizeof(ImVec2), sizeof(ImVec4), sizeof(ImDrawVert), sizeof(ImDrawIdx));
+void	imgui_init(t_sdl *sdl, t_imgui *imgui)
+{
+	igDebugCheckVersionAndDataLayout("1.84.1", \
+		sizeof(ImGuiIO), sizeof(ImGuiStyle), sizeof(ImVec2), sizeof(ImVec4), \
+		sizeof(ImDrawVert), sizeof(ImDrawIdx) \
+	);
 	imgui->ctx = igCreateContext(NULL);
-	ImGuiIO *io = igGetIO(); (void)io;
-	//io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	//io->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
-	// Setup Dear ImGui style
-	ImGuiStyle *style = igGetStyle();
-	igStyleColorsDark(style);
-	//igStyleColorsClassic(style);
-
-	// Setup Platform/Renderer backends
+	igStyleColorsDark(igGetStyle());
 	ImGui_ImplSDL2_InitForOpenGL(sdl->window, sdl->gl_context);
-	ImGui_ImplOpenGL3_Init(glsl_version);
-
-	// Load Fonts
-	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-	// - Read 'docs/FONTS.md' for more instructions and details.
-	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-	//io->Fonts->AddFontDefault();
-	//io->Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-	//io->Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-	//io->Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-	//io->Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-	//ImFont* font = io->Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-	//IM_ASSERT(font != NULL);
+	imgui_opengl_platform_init();
 }
 
 void	imgui_clear(t_imgui *imgui)
 {
-	// Cleanup
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplSDL2_Shutdown();
 	igDestroyContext(imgui->ctx);
