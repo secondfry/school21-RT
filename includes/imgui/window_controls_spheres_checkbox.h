8501d9f784def9f469005d64c625552e167b6b73
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-09-11 21:23:52 +0300

    Extract sphere checkboxes to separate file

---
+++ includes/imgui/window_controls_spheres_checkbox.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   window_controls_spheres_checkbox.h                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oadhesiv <secondfry+school21@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/11 21:20:43 by oadhesiv          #+#    #+#             */
+/*   Updated: 2021/09/11 21:21:31 by oadhesiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WINDOW_CONTROLS_SPHERES_CHECKBOX_H
+# define WINDOW_CONTROLS_SPHERES_CHECKBOX_H
+
+# include "rtv.h"
+
+void	imgui_sphere_local_checkboxes(t_rtv *rtv, t_byte idx);
+
+#endif
