9c605970384b5406b99b171dfccd80c84b600551
Rustam @SecondFry Gubaydullin <secondfry+github@gmail.com>
2021-09-12 14:24:03 +0300

    Add max to ig_drag_double

--- includes/imgui/controls.h
+++ includes/imgui/controls.h
@@ -6,7 +6,7 @@
 /*   By: oadhesiv <secondfry+school21@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 14:12:56 by oadhesiv          #+#    #+#             */
-/*   Updated: 2021/09/07 21:10:09 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2021/09/12 14:23:00 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 void	ig_image_defaults(ImTextureID user_texture_id, const ImVec2 size);
 bool	ig_drag_vector(const char *label, const double *v);
-bool	ig_drag_double(const char *label, const double *v);
+bool	ig_drag_double(const char *label, const double *v, const double max);
 bool	ig_group_top_default(const char *label);
 bool	ig_group_default(const char *label, bool first);
 
